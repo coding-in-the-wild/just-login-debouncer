6f651b3d6a66b9b01daa46ff29f889b4a54ec39b
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-14 19:20:35 -0500

    Added failing test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var parallel = require('run-parallel')
 var Debouncer = require('debouncer')
+var ms = require('ms')
 
 module.exports = function (core, db) {
 	var beginAuth = Object.create(core).beginAuthentication
-	var debounce = new Debouncer(debouncingDb, { //Untested :)
+	var debounce = new Debouncer(db, { //Untested :)
 		delayTimeMs: ['0 s', '5 s', '30 s', '5 m', '10 m', '30 m', '1 hr'].map(function (str) {
 			return ms(str)
 		})
@@ -13,14 +14,14 @@ module.exports = function (core, db) {
 		parallel({
 			email: function (done) {
 				debounce(emailAddress, function (err, allowed, remaining) {
-					done(err, {
+					done(err, err? null : {
 						allowed: allowed,
 						remaining: remaining
 					})
 				})
 			}, session: function (done) {
 				debounce(sessionId, function (err, allowed, remaining) {
-					done(err, {
+					done(err, err? null : {
 						allowed: allowed,
 						remaining: remaining
 					})
@@ -28,10 +29,10 @@ module.exports = function (core, db) {
 			}
 		},
 		function (err, result) {
-			if (!err) {
+			if (err) {
 				cb(err)
 			} else if (result.email.allowed && result.session.allowed) { //This is what we want
-				beginAuth(sessionId, emailAddress, cb)
+				beginAuth.call(null, null, sessionId, emailAddress, cb)
 			} else  { //Email and/or session debounce failed
 				var debounceError = new Error('Email and/or session debounce failure')
 				debounceError.debounce = true
--- test.js
+++ test.js
@@ -5,23 +5,48 @@ var level = require('level-mem')
 test('test debouncing', function (t) {
 	var sessionId = "session id not"
 	var emailAddress = "em@i.l"
-	var beginAuthCalls = 0;
-	var core = {
-		beginAuthentication: function () {
-			beginAuthCalls++
+	var coreThing = {
+		beginAuthCalls: 0,
+		beginAuthentication: function (err, sid, ea, cb) {
+			var calledWithDotCall = (typeof this.ArrayBuffer === 'function')
+			coreThing.beginAuthCalls++
+			console.log('args:',arguments)
+			if (typeof ea === 'function') {
+				ea(err, sid, calledWithDotCall)
+			} else {
+				cb(err, sid, ea, calledWithDotCall)
+			}
 		}
 	}
-	var coreCopy = Object.create(core)
-	var database = level('dbnc')
+	coreThing.beginAuthentication.call(null, null, {}, function (e, sid, ea, calledWithDotCall) {
+		t.notOk(e, 'no err')
+		t.ok(calledWithDotCall, 'was called with Fn.call(null...)')
+		coreThing.beginAuthentication(null, {}, function (e, sid, ea, calledWithDotCall) {
+			t.notOk(e, 'no err')
+			t.notOk(calledWithDotCall, 'was not called with Fn.call(null...)')
+			var core = Object.create(coreThing)
+			var coreCopy = Object.create(coreThing)
+			t.deepEqual(core, coreCopy, "core and copy started equal")
 
-	t.deepEqual(core, coreCopy, "core and copy started equal")
-	jlDebounce(core, database)
-	t.notDeepEqual(core, coreCopy, "jlDebounce modifed core")
-	core.beginAuthentication(sessionId, emailAddress, function (err, details) {
-		t.notOk(err, "no error")
-		details.allowed
-		details.remaining
+			var database = level('dbnc')
+			jlDebounce(core, database)
+			t.notDeepEqual(core, coreCopy, "jlDebounce modifed core")
+			core.beginAuthentication(sessionId, emailAddress, function (err, sid, ea, calledWithDotCall) {
+				t.ok(calledWithDotCall, 'was called with Fn.call(null...)')
+				t.notOk(err, "no error")
+				t.notOk(err && err.debounce, "no debounce error")
+				t.equal(sid, sessionId, 'has session id')
+				t.ok(ea, emailAddress, 'has email address')
+				core.beginAuthentication(sessionId, emailAddress, function (err, details, calledWithDotCall) {
+					t.ok(calledWithDotCall, 'was called with Fn.call(null...)')
+					t.ok(err, "no error")
+					t.ok(err && err.debounce, "no debounce error")
+					t.ok(details, 'has details')
+					t.ok(details && details.remaining, 'has details.remaining')
+					t.ok(details && details.allowed, 'has details.allowed')
+					t.end()
+				})
+			})
+		})
 	})
-
-	t.end()
 })
