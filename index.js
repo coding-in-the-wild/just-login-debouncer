17385a21bf29268ea84d125a67c88ae64d456ec6
Joseph Dykstra <josephdykstra@gmail.com>
2015-08-25 08:11:27 -0500

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 var parallel = require('run-parallel')
 var Debouncer = require('debouncer')
 var ms = require('ms')
+var delayTimes = ['0 s', '5 s', '30 s', '5 m', '10 m', '30 m', '1 hr'].map(ms)
 
 module.exports = function (core, db) {
 	var beginAuth = Object.create(core).beginAuthentication
-	var debounce = new Debouncer(db, {
-		delayTimeMs: ['0 s', '5 s', '30 s', '5 m', '10 m', '30 m', '1 hr'].map(function (str) {
-			return ms(str)
-		})
-	})
+	var debounce = new Debouncer(db, { delayTimeMs: delayTimes })
+
+	function debounceKey(key) {
+		return function (done) {
+			debounce(key, function (err, allowed, remaining) {
+				if (err) {
+					done(err)
+				} else {
+					done(null, {
+						allowed: allowed,
+						remaining: remaining
+					})
+				}
+			})
+		}
+	}
 
 	core.beginAuthentication = function beginAuthentication(sessionId, emailAddress, cb) {
 		parallel({
-			email: function (done) {
-				debounce(emailAddress, function (err, allowed, remaining) {
-					done(err, err ? null : {
-						allowed: allowed,
-						remaining: remaining
-					})
-				})
-			}, session: function (done) {
-				debounce(sessionId, function (err, allowed, remaining) {
-					done(err, err ? null : {
-						allowed: allowed,
-						remaining: remaining
-					})
-				})
-			}
-		},
-		function (err, result) {
+			email: debounceKey(emailAddress),
+			session: debounceKey(sessionId)
+		}, function (err, result) {
 			if (err) {
 				cb && cb(err)
-			} else if (result.email.allowed && result.session.allowed) { //This is what we want
+			} else if (result.email.allowed && result.session.allowed) { // This is what we want
 				beginAuth.call(null, sessionId, emailAddress, cb)
-			} else  { //Email and/or session debounce failed
+			} else {
 				var debounceError = new Error('Email and/or session debounce failure')
 				debounceError.debounce = true
 				cb && cb(debounceError, {
